0096b6826c68549e69833ddd64ed43e0a4700105
Isaac Connor <pseudo.connortechnology.com>
2013-10-03 11:19:59 -0400

    fix WriteJpeg def which got lost somehow

--- src/zm_image.h
+++ src/zm_image.h
@@ -203,7 +203,7 @@ public:
 	bool WriteRaw( const char *filename ) const;
 
 	bool ReadJpeg( const char *filename, unsigned int p_colours, unsigned int p_subpixelorder);
-	bool WriteJpeg( const char *filename, int quality_override=0 ) const;
+	bool WriteJpeg( const char *filename, int quality_override=0, bool on_blocking_abort=false ) const;
 	bool DecodeJpeg( const JOCTET *inbuffer, int inbuffer_size, unsigned int p_colours, unsigned int p_subpixelorder);
 	bool EncodeJpeg( JOCTET *outbuffer, int *outbuffer_size, int quality_override=0 ) const;
 
