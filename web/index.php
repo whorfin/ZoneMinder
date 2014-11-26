3717a7f16287b93cd0988e2d074ec178f5872825
Isaac Connor <iconnor@pseudo.connortechnology.com>
2014-11-26 16:26:04 -0500

    use ZM_SKIN_DEFAULT and ZM_CSS_DEFAULT, and fallback to classic if it isn't set

--- web/index.php
+++ web/index.php
@@ -63,6 +63,8 @@ if ( isset($_GET['skin']) )
     $skin = $_GET['skin'];
 elseif ( isset($_COOKIE['zmSkin']) )
     $skin = $_COOKIE['zmSkin'];
+elseif ZM_SKIN_DEFAULT
+	$skin = ZM_SKIN_DEFAULT
 else
     $skin = "classic";
 
@@ -70,6 +72,8 @@ if ( isset($_GET['css']) )
     $css = $_GET['css'];
 elseif ( isset($_COOKIE['zmCSS']) )
     $css = $_COOKIE['zmCSS'];
+elseif ZM_CSS_DEFAULT
+	$skin = ZM_CSS_DEFAULT
 else
     $css = "classic";
 
